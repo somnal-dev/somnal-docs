ee4e1056ef6331cb27346d786a2b92716722b680
솜놜 <90139018+somnal-dev@users.noreply.github.com>
2025-06-06 15:42:19 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,7 @@ const config = {
       algolia: {
         appId: 'T3QWX7B39S',
         apiKey: 'eb81e1902a31acff96b58c57a5886da3',
-        indexName: ''
+        indexName: 'somnal-docs'
       }
     }),
 };

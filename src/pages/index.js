38baab917b8b9db184c93a019ca0915bcaa8d6ef
솜놜 <90139018+somnwal@users.noreply.github.com>
2025-06-21 14:07:41 +0900

    20250621

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,40 +5,46 @@ import Layout from '@theme/Layout';
 import styles from './index.module.css';
 
 function Section({
-    text,
-    desc,
-    to
-}) {
-  return (
-      <a
-          className="pagination-nav__link text--center"
-          href={to}>
-        <h1 style={{ color: 'var(--ifm-color-emphasis-800)'}}>{text}</h1>
-        <div className="pagination-nav__label">{desc}</div>
-      </a>
-  )
+                     text,
+                     desc,
+                     to
+                 }) {
+    return (
+        <a
+            className="pagination-nav__link text--center"
+            href={to}>
+            <h1 style={{color: 'var(--ifm-color-emphasis-800)'}}>{text}</h1>
+            <div className="pagination-nav__label">{desc}</div>
+        </a>
+    )
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout>
-      <div
-          className="container"
-          style={{paddingTop:'30px', paddingLeft:'10vh', paddingRight:'10vh'}}
-      >
-        <Section
-          text={"안드로이드"}
-          desc={"문서보기"}
-          to={"docs/category/안드로이드"}
-        />
-        <br/>
-        <Section
-            text={"프론트엔드"}
-            desc={"문서보기"}
-            to={"docs/category/프론트엔드"}
-        />
-      </div>
-    </Layout>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout>
+            <div
+                className="container"
+                style={{paddingTop: '30px', paddingLeft: '10vh', paddingRight: '10vh'}}
+            >
+                <Section
+                    text={"Android"}
+                    desc={"문서보기"}
+                    to={"docs/category/android"}
+                />
+                <br/>
+                <Section
+                    text={"Frontend"}
+                    desc={"문서보기"}
+                    to={"docs/category/frontend"}
+                />
+                <br/>
+                <Section
+                    text={"Backend"}
+                    desc={"문서보기"}
+                    to={"docs/category/backend"}
+                />
+            </div>
+        </Layout>
+    );
 }
